ae9113ac2824e979bfebf806ac0f2eac958f2036
Haani Ansari <haani.ansari@uni-potsdam.de>
2025-08-28 21:06:09 +0200

    feat: added a functioning dtw kernal model, that is fast

--- src/scripts/sk_kernals.py
+++ src/scripts/sk_kernals.py
@@ -4,37 +4,48 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from scripts.kernals import d_DTW, d2
 from dtaidistance import dtw
 
+
 class dtw_SVC(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, C=1.0, gamma=0.01, estimate=False):
+    def __init__(self, C=1.0, gamma=0.01, estimate=False, inner_dist='euclidean'):
         self.C = C
         self.gamma = gamma
         self.svc_ = None
         self.X_train_ = None
         self.dist_mat_ = None
         self.estimate = estimate
+        self.inner_dist = inner_dist
+
+    def _build_gram_matrix(self, X, Y=None):
+        if Y is None:
+            Y = X
+        
+        t1, t2 = len(X), len(Y)
+        D = np.zeros((t1,t2), dtype=np.float64)
+
+        if self.estimate:
+            for i in range(t1):
+                for j in range(t2):
+                    D[i,j] = dtw.distance_fast(X[i], Y[j], inner_dist = self.inner_dist)
+        else:
+            for i in range(t1):
+                for j in range(t2):
+                    D[i,j] = dtw.distance(X[i], Y[j], inner_dist = self.inner_dist)
+        
+        K = np.exp(-self.gamma * D)
+        return K
 
     def fit(self, X, y):
         self.X_train_ = X
-        if self.estimate:
-            self.dist_mat_ = dtw.distance_matrix_fast(X)
-        else:
-            self.dist_mat_ = dtw.distance_matrix(X)
-        self.dist_mat_ = np.array(self.dist_mat_, dtype=np.float64)
-        K_train = np.exp(-self.gamma * self.dist_mat_)
-        
+        k_train = self._build_gram_matrix(X)
         self.svc_ = SVC(C=self.C, kernel="precomputed") 
-        self.svc_.fit(K_train, y)
+        self.svc_.fit(k_train, y)
+        self.classes_ = self.svc_.classes_  # forward classes_
         return self
     
     def predict(self, X):
-        if self.estimate:
-            D_predict = dtw.distance_matrix_fast(X, self.X_train_)
-        else:
-            D_predict = dtw.distance_matrix(X, self.X_train_)
-        D_predict = np.array(D_predict, dtype=np.float64)
-        K_predict = np.exp(-self.gamma * D_predict)
-        return self.svc_.predict(K_predict)
+        k_predict = self._build_gram_matrix(X, self.X_train_)
+        return self.svc_.predict(k_predict)
     
     def score(self, X, y):
         y_pred = self.predict(X)
